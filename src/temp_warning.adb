1112aa12d432571cdb89f3a2a1cdb558733472dd
mehdi <mbdj@free.fr>
2023-06-18 15:16:01 +0200

    beep selon temperature > < =

--- src/temp_warning.adb
+++ src/temp_warning.adb
@@ -1,17 +1,21 @@
 with Pico;
 with RP.Clock;
-with RP.GPIO;       use RP.GPIO;
+with RP.GPIO;
 with RP.Device;
-with RP.ADC;
+with RP.ADC; use RP.ADC;
 
 with HAL.UART;
 with RP.UART;
 with Ada.Characters.Latin_1;  --  for New_Line
 
 procedure Temp_Warning is
+   use RP.GPIO;
 
-   --  temperature
-   Temperature : RP.ADC.Celsius;
+   --  nombre de mesures lues à la hausse avant l'alerte
+   Nombre_De_Mesures                              : constant Natural := 9;
+
+   Temperature, Derniere_Temperature_De_Reference : RP.ADC.Celsius;
+   Compteur_Elevation_Temperature                 : Natural := Nombre_De_Mesures;
 
    Buzzer : GPIO_Point renames Pico.GP2;
 
@@ -29,51 +33,108 @@ procedure Temp_Warning is
    Status  : HAL.UART.UART_Status;
 
 
-   --------------------
-   -- String_Message --
-   --------------------
 
-   function String_Message (T : RP.ADC.Celsius) return Type_Message is
-      Msg : Type_Message;
+   procedure Afficher_Temperature (Temp       : in RP.ADC.Celsius;
+                                   Data       : in HAL.UART.UART_Data_8b;
+                                   Status     : out HAL.UART.UART_Status) is
+
+      function String_Message (T : RP.ADC.Celsius) return Type_Message is
+         use RP.ADC;
+         Msg : Type_Message;
+      begin
+         case T is
+         when 17 => Msg := Type_Message ("17" & New_Line);
+         when 18 => Msg := Type_Message ("18" & New_Line);
+         when 19 => Msg := Type_Message ("19" & New_Line);
+         when 20 => Msg := Type_Message ("20" & New_Line);
+         when 21 => Msg := Type_Message ("21" & New_Line);
+         when 22 => Msg := Type_Message ("22" & New_Line);
+         when 23 => Msg := Type_Message ("23" & New_Line);
+         when 24 => Msg := Type_Message ("24" & New_Line);
+         when 25 => Msg := Type_Message ("25" & New_Line);
+         when 26 => Msg := Type_Message ("26" & New_Line);
+         when 27 => Msg := Type_Message ("27" & New_Line);
+         when 28 => Msg := Type_Message ("28" & New_Line);
+         when 29 => Msg := Type_Message ("29" & New_Line);
+         when 30 => Msg := Type_Message ("30" & New_Line);
+         when 31 => Msg := Type_Message ("31" & New_Line);
+         when 32 => Msg := Type_Message ("32" & New_Line);
+         when 33 => Msg := Type_Message ("33" & New_Line);
+         when 34 => Msg := Type_Message ("34" & New_Line);
+         when 35 => Msg := Type_Message ("35" & New_Line);
+         when 36 => Msg := Type_Message ("36" & New_Line);
+         when 37 => Msg := Type_Message ("37" & New_Line);
+         when 38 => Msg := Type_Message ("38" & New_Line);
+         when 39 => Msg := Type_Message ("39" & New_Line);
+         when others => Msg := Type_Message ("99" & New_Line);
+         end case;
+
+         if T < 17 then
+            Msg := Type_Message ("<<" & New_Line);
+         elsif T > 39 then
+            Msg := Type_Message (">>" & New_Line);
+         end if;
+
+         return Msg;
+
+      end String_Message;
+
    begin
-      case T is
-      when 17 => Msg := Type_Message ("17" & New_Line);
-      when 18 => Msg := Type_Message ("18" & New_Line);
-      when 19 => Msg := Type_Message ("19" & New_Line);
-      when 20 => Msg := Type_Message ("20" & New_Line);
-      when 21 => Msg := Type_Message ("21" & New_Line);
-      when 22 => Msg := Type_Message ("22" & New_Line);
-      when 23 => Msg := Type_Message ("23" & New_Line);
-      when 24 => Msg := Type_Message ("24" & New_Line);
-      when 25 => Msg := Type_Message ("25" & New_Line);
-      when 26 => Msg := Type_Message ("26" & New_Line);
-      when 27 => Msg := Type_Message ("27" & New_Line);
-      when 28 => Msg := Type_Message ("28" & New_Line);
-      when 29 => Msg := Type_Message ("29" & New_Line);
-      when 30 => Msg := Type_Message ("30" & New_Line);
-      when 31 => Msg := Type_Message ("31" & New_Line);
-      when 32 => Msg := Type_Message ("32" & New_Line);
-      when 33 => Msg := Type_Message ("33" & New_Line);
-      when 34 => Msg := Type_Message ("34" & New_Line);
-      when 35 => Msg := Type_Message ("35" & New_Line);
-      when 36 => Msg := Type_Message ("36" & New_Line);
-      when 37 => Msg := Type_Message ("37" & New_Line);
-      when 38 => Msg := Type_Message ("38" & New_Line);
-      when 39 => Msg := Type_Message ("39" & New_Line);
-      when others => Msg := Type_Message ("99" & New_Line);
+      Message := String_Message (Temp);
+      Port.Transmit (Data, Status, Timeout => 0);
+   end;
+
+   procedure Afficher (Msg        : in Type_Message;
+                       Data       : in HAL.UART.UART_Data_8b;
+                       Status     : out HAL.UART.UART_Status) is
+   begin
+      Message := Msg;
+      Port.Transmit (Data, Status, Timeout => 0);
+   end;
+
+   procedure Afficher_Compteur (N          : in Integer;
+                                Data       : in HAL.UART.UART_Data_8b;
+                                Status     : out HAL.UART.UART_Status) is
+   begin
+      case N is
+         when 0 => Message := Type_Message ("0!" & New_Line);
+         when 1 => Message := Type_Message ("1!" & New_Line);
+         when 2 => Message := Type_Message ("2!" & New_Line);
+         when 3 => Message := Type_Message ("3!" & New_Line);
+         when 4 => Message := Type_Message ("4!" & New_Line);
+         when 5 => Message := Type_Message ("5!" & New_Line);
+         when 6 => Message := Type_Message ("6!" & New_Line);
+         when 7 => Message := Type_Message ("7!" & New_Line);
+         when 8 => Message := Type_Message ("8!" & New_Line);
+         when 9 => Message := Type_Message ("9!" & New_Line);
+         when others => Message := Type_Message ("ER" & New_Line);
       end case;
 
-      if Integer (Temperature) < 17 then
-         Msg := Type_Message ("<<" & New_Line);
-      elsif Integer (Temperature) > 39 then
-         Msg := Type_Message (">>" & New_Line);
-      end if;
-
-      return Msg;
-
-   end String_Message;
+      Port.Transmit (Data, Status, Timeout => 0);
+   end;
 
 
+   -- plusieurs lectures sont faites et la moyenne est retournée
+   function Read_Temperature return RP.ADC.Celsius is
+      use RP.ADC;
+      T : RP.ADC.Celsius := 0;
+   begin
+      for I in 1 .. 20 loop
+         T := T + RP.ADC.Temperature;
+         RP.Device.Timer.Delay_Milliseconds (500);
+      end loop;
+
+      return RP.ADC.Celsius (Integer (T) / 20);
+   end Read_Temperature;
+
+
+   procedure Beep (Ms : Integer := 100) is
+   begin
+      -- beep
+      Buzzer.Toggle;
+      RP.Device.Timer.Delay_Milliseconds (Ms);
+      Buzzer.Toggle;
+   end;
 
 begin
    --
@@ -103,14 +164,63 @@ begin
    --  The default config is 115200 8n1, this example just overrides the baud rate
    Port.Configure (Config => (Baud => 115200, others => <>));
 
+   Afficher (Msg    => Type_Message ("==" & New_Line),
+             Data   => Data,
+             Status => Status);
+
+   --  au démarrage on attend que la température se stabilise car le Pico qui vient d'être
+   --  branché va voir sa température augmenter
+   for I in 1 .. 2 loop
+      Temperature := Read_Temperature;
+
+      Afficher_Temperature (Temp   => Temperature,
+                            Data   => Data,
+                            Status => Status);
+      RP.Device.Timer.Delay_Milliseconds (1000);
+   end loop;
+
+   Afficher (Msg    => Type_Message ("--" & New_Line),
+             Data   => Data,
+             Status => Status);
+
+   Derniere_Temperature_De_Reference := Read_Temperature;
+
+   Afficher_Temperature (Temp   => Derniere_Temperature_De_Reference,
+                         Data   => Data,
+                         Status => Status);
 
    loop
-      Temperature := RP.ADC.Temperature;
-
-      Message := String_Message (Temperature);
-
-      Port.Transmit (Data, Status, Timeout => 0);
       RP.Device.Timer.Delay_Milliseconds (500);
+      Temperature := Read_Temperature;
+
+      Afficher_Temperature (Temp   => Temperature,
+                            Data   => Data,
+                            Status => Status);
+
+      if Temperature < Derniere_Temperature_De_Reference then
+         Derniere_Temperature_De_Reference := Temperature;
+         Compteur_Elevation_Temperature := Nombre_De_Mesures;
+
+      elsif Temperature = Derniere_Temperature_De_Reference then
+         Derniere_Temperature_De_Reference := Temperature;
+         Compteur_Elevation_Temperature := Nombre_De_Mesures;
+
+         Beep;
+
+      else --  Temperature > Derniere_Temperature_De_Reference
+         Compteur_Elevation_Temperature := @ -1;
+         Beep;
+         RP.Device.Timer.Delay_Milliseconds (100);
+         Beep;
+
+      end if;
+
+      Afficher_Compteur (Compteur_Elevation_Temperature,
+                         Data   => Data,
+                         Status => Status);
+
+      exit when Compteur_Elevation_Temperature = 0;
+
    end loop;
 
    --  triggers buzzer warning
